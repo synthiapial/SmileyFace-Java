d81de009d42880cfd4daca2dd861c614b2a567dd
Synthia Pial <synthiakabir98@gmail.com>
2018-09-03 10:03:46 -0400

    Testing

---
+++ TestSmilingFace.java
@@ -0,0 +1,13 @@
+
+public class TestSmilingFace {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        new SmilingFace();
+
+    }
+
+}
